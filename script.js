94dad26e4c56742dd418a982dd21b69faa84513e
Dan-246 <tyusuke903@gmail.com>
2024-06-07 13:48:13 +0900

    timer

--- script.js
+++ script.js
@@ -6,24 +6,16 @@ var timeId = setInterval(() => {
 
   // 差分を求める
   var diff = currentTime - oldTime;
-
   var diffSec = totalTime - diff;
 
   //ミリ秒を整数に変換
   var remainSec = diffSec / 1000;
 
+  var text = remainSec;
   
-  
-
-
-  //diffSec = diffSec.slice(0,2) + ":" +diffSec.slice(-3)
-
-  let text = `残り${remainSec}秒`;
-
   // 0秒以下になったら
   if (diffSec <= 0) {
     clearInterval(timeId);
-
     // タイマー終了の文言を表示する
     text = "終了";
   }
