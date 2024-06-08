d90c4c2609c436fcc90758cac262095073aaca0b
Dan-246 <tyusuke903@gmail.com>
2024-06-08 23:57:37 +0900

    調整

---
+++ def.py
@@ -0,0 +1,10 @@
+"""def keisan(num):    #
+    result = num * 3 + 2
+    return result   #関数を使わないといけないことを実演するのはわかりやすい・
+print(keisan(131))            #今まで使った関数と関連させていてわかりやすい"""
+
+def heikin(math,eng,jp,rika):
+    avg = (math+eng+jp+rika)/4
+    return avg
+
+print(heikin(65,87,89,67))
